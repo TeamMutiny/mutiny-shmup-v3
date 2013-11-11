be542eda4dfc933d52e6697529ad3f28f3cecd8c
Ville Touronen <touronen@gmail.com>
2013-11-11 22:10:52 +0200

    Added explosion for bullets. It uses a generic ExplosionOnDestroy script, which can be reused on any object.

---
+++ Assets/ExplosionOnDestroy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionOnDestroy : MonoBehaviour {
+	
+	public GameObject explosion;
+	private bool isQuitting = false;
+	
+	void OnApplicationQuit()
+	{
+	    isQuitting = true;
+	}
+	
+	void OnDestroy()
+	{
+		if(!isQuitting)
+		{	
+		GameObject newExplosion = Instantiate(explosion,transform.position,transform.rotation) as GameObject;
+		Destroy(newExplosion,2);
+		}
+	}
+}
