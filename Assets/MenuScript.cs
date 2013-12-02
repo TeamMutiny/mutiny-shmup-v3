7c60bc7daa24ccba18ce18ae3113d1bca2beef30
Ville Touronen <touronen@gmail.com>
2013-12-02 22:07:31 +0200

    Some stuff in the menu. -v

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI() {
+		GUI.skin = null;
+		if(GUI.Button(new Rect(20,40,80,20), "Play")) {
+			// New game
+		}
+
+		// Make the second button.
+		if(GUI.Button(new Rect(20,70,80,20), "Quit")) {
+			// Quit
+		}
+	}
+}
